3aaaef50102674bb4052cc7a7e172d17d9e2b0f0
omelnykov1 <sashqa.melnikov@gmail.com>
2020-07-14 16:05:24 -0400

    fix for mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,14 +43,7 @@
 	// Nav.
 
 		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
+	
 		// Panel.
 			$(
 				'<div id="navPanel">' +
